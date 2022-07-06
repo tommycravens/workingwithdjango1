58e7a488f83367991a21d666910f012c3b5cdedf
tommycravens <joe.t.cravens92@gmail.com>
2022-07-06 13:07:03 -0400

    import render and def detail

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,8 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.http import Http404
+from django.shortcuts import render
 
 from .models import Question
 
@@ -32,3 +34,10 @@ def index(request):
         'latest_question_list': latest_question_list,
     }
     return HttpResponse(template.render(context, request))
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
