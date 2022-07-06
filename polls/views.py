e868d4e5f6a4ed519dde3a9d272987c88dc997fd
tommycravens <joe.t.cravens92@gmail.com>
2022-07-06 12:27:03 -0400

    added the loader view into views file

--- polls/views.py
+++ polls/views.py
@@ -24,3 +24,11 @@ def index(request):
   latest_question_list = Question.objects.order_by('-pub_date')[:5]
   output = ', '.join([q.question_text for q in latest_question_list])
   return HttpResponse(output)
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
