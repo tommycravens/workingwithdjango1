877c7964172e171d3d5066d0578cf895cdd04f58
tommycravens <joe.t.cravens92@gmail.com>
2022-07-06 11:47:23 -0400

    Imported models to views and added another view

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 
 
@@ -16,3 +17,8 @@ def results(request, question_id):
 
 def vote(request, question_id):
   return HttpResponse("You're voting on question %s." % question_id)
+
+def index(request):
+  latest_question_list = Question.objects.order_by('-pub_date')[:5]
+  output = ', '.join([q.question_text for q in latest_question_list])
+  return HttpResponse(output)
