aadc4fc410e3a5a46154d3a1a0c24992fbfb251a
tommycravens <joe.t.cravens92@gmail.com>
2022-07-08 08:30:59 -0400

    fixing models.py to work correctly

--- polls/models.py
+++ polls/models.py
@@ -3,9 +3,6 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
-def was_published_recently(self):
-    now = timezone.now()
-    return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Question(models.Model):
   question_text = models.CharField(max_length=200)
@@ -13,7 +10,10 @@ class Question(models.Model):
   def __str__(self):
     return self.question_text
   def was_published_recently(self):
-    return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    now = timezone.now()
+    return now - datetime.timedelta(days=1) <= self.pub_date <= now
+  # def was_published_recently(self):
+  #   return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
   question = models.ForeignKey(Question, on_delete=models.CASCADE)
