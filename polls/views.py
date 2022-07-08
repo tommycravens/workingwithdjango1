73931d3226856ede1ee99e84d883f1ffb184936d
tommycravens <joe.t.cravens92@gmail.com>
2022-07-08 13:35:15 -0400

    took out test because it broke the site.

--- polls/views.py
+++ polls/views.py
@@ -34,14 +34,14 @@ class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
 
-    def get_queryset(self):
-      """
-      Return the last five published questions (not including those set to be
-      published in the future).
-      """
-      return Question.objects.filter(
-          pub_date__lte=timezone.now()
-      ).order_by('-pub_date')[:5]
+    # def get_queryset(self):
+    #   """
+    #   Return the last five published questions (not including those set to be
+    #   published in the future).
+    #   """
+    #   return Question.objects.filter(
+    #       pub_date__lte=timezone.now()
+    #   ).order_by('-pub_date')[:5]
 
 
 def vote(request, question_id):
