2091eae424cf460df1205b4f5b248a0229a08fd9
tommycravens <joe.t.cravens92@gmail.com>
2022-07-11 15:57:49 -0400

    trying to finish this last portion in models.py

--- polls/models.py
+++ polls/models.py
@@ -14,14 +14,15 @@ class Question(models.Model):
     return now - datetime.timedelta(days=1) <= self.pub_date <= now
   # def was_published_recently(self):
   #   return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-  @admin.display(
-        boolean=True,
-        ordering='pub_date',
-        description='Published recently?',
-    )
-  def was_published_recently(self):
-      now = timezone.now()
-      return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+  # @admin.display(
+  #       boolean=True,
+  #       ordering='pub_date',
+  #       description='Published recently?',
+  #   )
+  # def was_published_recently(self):
+  #       now = timezone.now()
+  #       return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Choice(models.Model):
   question = models.ForeignKey(Question, on_delete=models.CASCADE)
