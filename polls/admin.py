9877d1a4e26fc73b5d90bd250904fe9e0a7da369
tommycravens <joe.t.cravens92@gmail.com>
2022-07-11 14:57:38 -0400

    added class for choice

--- polls/admin.py
+++ polls/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 
 from .models import Choice, Question
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
