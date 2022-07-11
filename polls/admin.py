536639d857b580f4cb6c5fa91a5414eb4a73d899
tommycravens <joe.t.cravens92@gmail.com>
2022-07-11 15:50:33 -0400

    fixing errors, but may have an error in models.py

--- polls/admin.py
+++ polls/admin.py
@@ -15,13 +15,5 @@ class QuestionAdmin(admin.ModelAdmin):
     ]
     inlines = [ChoiceInline]
     list_display = ('question_text', 'pub_date','was_published_recently')
-    @admin.display(
-        boolean=True,
-        ordering='pub_date',
-        description='Published recently?',
-    )
-    def was_published_recently(self):
-        now = timezone.now()
-        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 admin.site.register(Question, QuestionAdmin)
