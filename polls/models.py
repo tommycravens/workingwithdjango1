03724b656461eb89d994e9f0cafb85595e4e10eb
tommycravens <joe.t.cravens92@gmail.com>
2022-07-11 16:07:56 -0400

    fixing models.py by importing the admin file

--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,7 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 # Create your models here.
 
@@ -15,14 +16,14 @@ class Question(models.Model):
   # def was_published_recently(self):
   #   return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
-  # @admin.display(
-  #       boolean=True,
-  #       ordering='pub_date',
-  #       description='Published recently?',
-  #   )
-  # def was_published_recently(self):
-  #       now = timezone.now()
-  #       return now - datetime.timedelta(days=1) <= self.pub_date <= now
+  @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
+  def was_published_recently(self):
+      now = timezone.now()
+      return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Choice(models.Model):
   question = models.ForeignKey(Question, on_delete=models.CASCADE)
