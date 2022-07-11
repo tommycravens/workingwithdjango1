bdc1ecbc832a7b6a2c0dc97cc2a7ced66a50ba86
tommycravens <joe.t.cravens92@gmail.com>
2022-07-11 14:55:13 -0400

    fixing errors in admin page

--- polls/admin.py
+++ polls/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 
 from .models import Choice, Question
-# Register your models here.
-class QuestionAdmin(admin.ModelAdmin):
-  fieldsets = [
-    (None,     {'fields': ['pub_date', 'question_text']})
-    ('Date information', {'fields': ['pub_date']})
-  ]
 
-admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
